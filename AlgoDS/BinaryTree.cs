81ecaa8e7ebff0f1a04589f95af900bd0a9a3170
Kalyan <Kalyan.Sutapalli@hotmail.com>
2017-10-25 21:48:11 -0400

    Binary Tree Partial

---
+++ AlgoDS/BinaryTree.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoDS
+{
+    class BinaryTree<T> : IEnumerable<T>
+        where T : IComparable<T>
+    {
+
+        public BinaryTreeNode<T> _head;
+        public int _count;
+
+        public void Add(T value)
+        {
+            if (_head == null)
+                _head = new BinaryTreeNode<T>(value);
+            else
+                AddTo(_head, value);
+            _count++;
+
+        }
+
+        private void AddTo(BinaryTreeNode<T> node, T value)
+        {
+            if (value.CompareTo(node.Value) < 0)
+            {
+                if (node.Left == null)
+                    node.Left = new BinaryTreeNode<T>(value);
+                else
+                    AddTo(node.Left, value);
+            }
+            else
+            {
+                if (node.Right == null)
+                    node.Right = new BinaryTreeNode<T>(value);
+                else
+                    AddTo(node.Right, value);
+            }
+        }
+
+        public bool Contains(T value)
+        {
+            BinaryTreeNode<T> parent;
+            return FindWithParent(value, out parent) != null;
+        }
+
+        private BinaryTreeNode<T> FindWithParent(T value, out BinaryTreeNode<T> parent)
+        {
+            BinaryTreeNode<T> current = _head;
+            parent = null;
+            while (current != null)
+            {
+                int result = current.CompareTo(value);
+
+                if (result < 0)
+                {
+                    parent = current;
+                    current = current.Left;
+                }
+                else if (result > 0)
+                {
+                    parent = current;
+                    current = current.Right;
+                }
+                else
+                    break;
+            }
+            return current;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
